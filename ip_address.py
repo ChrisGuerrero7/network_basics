2824825aa3a15d04c85bc2fad86c0b34987d2c11
Chris Guerrero <chrisguerrero.js@gmail.com>
2021-07-19 13:06:08 -0500

    Se agrega una funcion de verificacion de direcciones ip

--- ip_address.py
+++ ip_address.py
@@ -1,7 +1,31 @@
 from binary import to_decimal, to_binary
 
 
+def is_ip_decimal(ip):
+    """
+    Esta funcion se encarga de verificar si la direccion ip ingresada es correcta.
+    """
+    try:
+        ip_list = ip.split(sep='.')
+        count = 0
+        for num in ip_list:
+            if int(num) <= 255:
+                continue
+            else:
+                count += 1
+        
+        if len(ip_list) == 4 and count == 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+
 def binary_notation(ip_decimal):
+    """
+    Esta funcion se encarga de convertir la direccion ip decimal en notacion binaria.
+    """
     ip_decimal_list = ip_decimal.split(sep='.')
     ip_binary_list = []
     for decimal in ip_decimal_list:
@@ -11,8 +35,13 @@ def binary_notation(ip_decimal):
 
 def run():
     ip_decimal = input("Indica la direccion IP en decimal: ")
-    binary_list = binary_notation(ip_decimal)
-    print('.'.join(binary_list))
+    is_ip = is_ip_decimal(ip_decimal)
+    if is_ip == True:
+        binary_list = binary_notation(ip_decimal)
+        binary_str = '.'.join(binary_list)
+        print('Notacion Binaria: ' + binary_str)
+    else:
+        print('La direccion ingresada no es correcta. Intenta nuevamente')
 
 
 if __name__ == '__main__':
