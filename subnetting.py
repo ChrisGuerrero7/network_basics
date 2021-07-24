2207df405dafab8f3b786e28d04746e585dd6e62
Chris Guerrero <chrisguerrero.js@gmail.com>
2021-07-23 22:32:27 -0500

    Bosquejo de funcionalidad de network address

--- subnetting.py
+++ subnetting.py
@@ -1,8 +1,18 @@
+def network(address, mask):
+    """
+    This function allows us to find the network address from the address of a host.
+    """
+
+
 def run():
     """
     This program allows us to design the fixed size network (FLSM)
     """
-    pass
+    print("SUBNETTING - FLSM")
+    address = input("Indica la direccion IP: ")
+    prefix_length = input("Indica la mascara de red: ")
+    network_add = network(address, prefix_length)
+    print(network_add)
 
 
 if __name__ == '__main__':
