bf3c23b34cbc57a54f5434c4b202a12cf909a7c4
Chris Guerrero <chrisguerrero.js@gmail.com>
2021-07-23 22:50:45 -0500

    Se borra la funcionalidad y añadimos verificacion de direccion y prefijo de longitud

--- subnetting.py
+++ subnetting.py
@@ -1,18 +1,16 @@
-def network(address, mask):
-    """
-    This function allows us to find the network address from the address of a host.
-    """
+from ip_address import is_ip_decimal
 
 
 def run():
     """
-    This program allows us to design the fixed size network (FLSM)
+    This program allows us to design the fixed size network (FLSM).
     """
     print("SUBNETTING - FLSM")
     address = input("Indica la direccion IP: ")
-    prefix_length = input("Indica la mascara de red: ")
-    network_add = network(address, prefix_length)
-    print(network_add)
+    prefix_length = input("Indica el prefijo de longitud: ")
+    is_address = is_ip_decimal(address)
+    if is_address == True and int(prefix_length) <= 32:
+        pass
 
 
 if __name__ == '__main__':
