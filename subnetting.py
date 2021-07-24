ed3f53e6772efc65c751e2039519204f408ad921
Chris Guerrero <chrisguerrero.js@gmail.com>
2021-07-23 23:43:57 -0500

    Funcionalidad que encuentra la direccion de red pero solo para clase C

--- subnetting.py
+++ subnetting.py
@@ -1,4 +1,30 @@
-from ip_address import is_ip_decimal
+from ip_address import *
+
+
+def network(ip, mask):
+    """
+    This function allows us to find the address from the address of a host.
+    """
+    class_ip = class_decimal(ip)[0]
+    pl = class_decimal(ip)[1]
+    binary_ip = binary_notation(ip)
+    binary_list = []
+    count = 0
+    if class_ip == 'C':
+        binary = binary_ip[3]
+        borrowed_bit = int(mask) - pl
+        for bit in binary:
+            count += 1
+            if count <= borrowed_bit:
+                binary_list.append(bit)
+            else:
+                binary_list.append('0')
+        binary = ''.join(binary_list)
+        binary_ip[3] = binary
+        binary_join = '.'.join(binary_ip)
+        ip_network_list = dotted_decimal(binary_join)
+        ip_network = '.'.join(ip_network_list)
+    return ip_network
 
 
 def run():
@@ -10,7 +36,10 @@ def run():
     prefix_length = input("Indica el prefijo de longitud: ")
     is_address = is_ip_decimal(address)
     if is_address == True and int(prefix_length) <= 32:
-        pass
+        network_add = network(address, prefix_length)
+        print(network_add)
+    else:
+        print("Direccion IP o Mascara de red incorrecta.")
 
 
 if __name__ == '__main__':
