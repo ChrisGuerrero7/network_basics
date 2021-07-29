d5914fb329094663c7506e2b32338fa850bc605e
Chris Guerrero <chrisguerrero.js@gmail.com>
2021-07-29 14:51:14 -0500

    Se añade funcionalidad donde se halla el prefijo de longitud

--- fixed_subnet.py
+++ fixed_subnet.py
@@ -1,14 +1,33 @@
+from ip_address import class_decimal, netmask
+
+
 def get_prefix(ip, n_host):
-    pass
+    """
+    This function allows us to find the netmask from the network address and the hosts number.
+    """
+    pl = 32 - class_decimal(ip)[1]
+    potential_list_host = {}
+
+    for i in range(pl+1):
+        class_host = (2 ** i) - 2
+        if class_host >= n_host:
+            potential_list_host[class_host] = i
+
+    potential_host = min(potential_list_host.keys())
+    bit_host = potential_list_host[potential_host]
+    prefix = 32 - bit_host
+    return prefix
 
 
 def run():
-    print("FLSM")
-    print("Hallamos la mascara de sebred a partir de la direccion de red y el numero de host.\n")
+    print("FLSM - Netmask")
     network = input("Indica la direccion de red: ")
     num_host = int(input("Indica el numero de host: "))
     prefix_length = get_prefix(network, num_host)
-    print(prefix_length)
+    mask_list = netmask(prefix_length)
+    mask = '.'.join(mask_list)
+    print("Prefijo de longitud: " + str(prefix_length))
+    print("Netmask: " + mask)
 
 
 if __name__ == '__main__':
